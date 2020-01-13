9a931578a4f23c6ac6a377773fd277bf44433c99
innovation <578777205@qq.com>
2020-01-13 14:58:17 +0800

    add web socket

--- src/main/java/com/lck/whoisthespy/controller/GameUserController.java
+++ src/main/java/com/lck/whoisthespy/controller/GameUserController.java
@@ -4,6 +4,7 @@ import com.lck.whoisthespy.service.GameUserService;
 import com.lck.whoisthespy.vo.ResponseVO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class GameUserController {
     GameUserService gameUserService;
 
     @ApiOperation("获取所有游戏玩家")
+    @GetMapping("")
     public ResponseVO getAllGameUser(){
         return gameUserService.getAllGameUser();
     }
